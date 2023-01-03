eb75d78267b119cc6e1342266e93c038c3273255
midCenturyDude7 <mgriffes@gmail.com>
2023-01-03 10:04:24 -0800

    Finished styling Hero component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,10 +4,10 @@ import heroImage from "../img/photo-grid.png";
 export default function Hero() {
 
   return (
-    <main className="main--container">
-      <img className="main--hero-img" src={heroImage} alt="Image grid of various online experiences" />
-      <h1 className="main--title">Online Experiences</h1>
-      <p className="main--text">Join unique interactive activities led by one-of-a-kind hosts&#8212;all without leaving home.</p>
-    </main>
+    <section className="hero--container">
+      <img className="hero--img" src={heroImage} alt="Image grid of various online experiences" />
+      <h1 className="hero--title">Online Experiences</h1>
+      <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts&#8212;all without leaving home.</p>
+    </section>
   );
 }
